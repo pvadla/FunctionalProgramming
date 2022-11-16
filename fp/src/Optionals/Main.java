47ab9142061cab66f9fb47c3437f6babfda46dbf
pvadla <praveen454griet@gmail.com>
2022-11-16 20:21:32 +0530

    1115

---
+++ fp/src/Optionals/Main.java
@@ -0,0 +1,29 @@
+package Optionals;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        Object o = Optional.ofNullable("Hello")
+                .orElseGet(() -> "default value");
+
+        System.out.println(o);
+
+        Object o1 = Optional.ofNullable("Hi")
+                .orElseThrow(IllegalStateException::new);
+        System.out.println(o1);
+
+        Optional.ofNullable(null)
+                .ifPresent(System.out::println);
+
+        Optional.ofNullable("john@gmail.com")
+                .ifPresentOrElse(
+                        System.out::println,
+                        () -> {
+                            System.out.println("Cannot send email");
+                        });
+
+
+    }
+}
---
+++ fp/src/streams/_Stream.java
@@ -0,0 +1,51 @@
+package streams;
+
+import imperative.Main;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
+public class _Stream {
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("JOHN", MALE),
+                new Person("PRAVEEN", MALE),
+                new Person("ISHANVI", FEMALE),
+                new Person("VIJAY", MALE),
+                new Person("JOHN2", MALE),
+                new Person("BOB", PREFER_NOT_TO_SAY)
+        );
+
+        people.stream()
+                .map(person -> person.name)
+                .mapToInt(String::length)
+                .forEach(System.out::println);
+
+    }
+
+    static class Person{
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender{
+        MALE, FEMALE, PREFER_NOT_TO_SAY
+    }
+}
